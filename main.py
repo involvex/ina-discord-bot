82bba5ffbb01e2929332fdc06a686a8b8d144d52
Involvex <lukaswill97@gmail.com>
2025-06-19 02:21:40 +0200

    up

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ from dotenv import load_dotenv
 import datetime # For timestamps in logs
 load_dotenv()
 
-__version__ = "0.2.105" 
+__version__ = "0.2.106" 
 
 logging.basicConfig(
     level=logging.DEBUG, # Temporarily change to DEBUG to see more detailed update check logs
